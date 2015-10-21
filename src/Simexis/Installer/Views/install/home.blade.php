064854296f6758ab198a7529b951db17fdf5af2c
Georgi Nachev <g.nachev@webmedia.bg>
2015-10-21 16:09:51 +0300

    Fix view missing

--- src/Simexis/Installer/Views/install/home.blade.php
+++ src/Simexis/Installer/Views/install/home.blade.php
@@ -4,31 +4,31 @@
     <div class="panel panel-success">
         <div class="panel-heading">
             <h3 class="panel-title">
-                <i class="glyphicon glyphicon-file"></i>
-                @lang('installer::installer.permissions.title')
+                <i class="glyphicon glyphicon-exclamation-sign"></i>
+                @lang('installer::installer.requirements.title')
             </h3>
         </div>
         <div class="panel-body">
             <div class="bs-component">
                 <ul class="list-group">
-                    @foreach($permissions['permissions'] as $permission)
-                        <li class="list-group-item">
-                            @if($permission['isSet'])
-                                <span class="badge badge-success">
-                                    @lang('installer::installer.' . $permission['permission'])
-                                </span>
-                            @else
-                                <span class="badge badge-danger">
-                                    @lang('installer::installer.' . $permission['permission'])
-                                </span>
-                            @endif
-                            {{ $permission['folder'] }}
-                        </li>
+                    @foreach($requirements['requirements'] as $element => $enabled)
+                    <li class="list-group-item">
+                        @if($enabled)
+                            <span class="badge badge-success">
+                                <i class="glyphicon glyphicon-ok"></i>
+                            </span>
+                        @else
+                            <span class="badge badge-danger">
+                                <i class="glyphicon glyphicon-remove"></i>
+                            </span>
+                        @endif
+                        {{ $element }}
+                    </li>
                     @endforeach
                 </ul>
             </div>
-            @if(!isset($permissions['errors']))
-                <a class="btn btn-success" href="{{ route('installer::database') }}">
+            @if(!isset($requirements['errors']))
+                <a class="btn btn-success" href="{{ route('installer::permissions') }}">
                     @lang('installer::installer.next')
                 </a>
             @endif
