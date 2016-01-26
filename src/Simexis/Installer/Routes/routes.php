c22cde26c239f12bd8fcbfb54396e9ffcf615f0a
Georgi Nachev <g.nachev@webmedia.bg>
2016-01-26 12:31:12 +0200

    Fix routes

--- src/Simexis/Installer/Routes/routes.php
+++ src/Simexis/Installer/Routes/routes.php
@@ -1,5 +1,9 @@
 <?php
-Route::group(['prefix' => 'Installer@install', 'namespace' => 'Simexis\Installer\Controllers\Install'], function()
+$middleware = [];
+if(version_compare(app()->version(), '5.2', '>=')) 
+	$middleware = ['middleware' => ['web']];
+
+Route::group($middleware + ['prefix' => 'Installer@install', 'namespace' => 'Simexis\Installer\Controllers\Install'], function()
 {
 	Route::group(['middleware' => 'installerCanInstall'], function(){
 		Route::get('/', [
@@ -25,7 +29,7 @@ Route::group(['prefix' => 'Installer@install', 'namespace' => 'Simexis\Installer
 	]);
 });
 
-Route::group(['prefix' => 'Installer@update', 'namespace' => 'Simexis\Installer\Controllers\Update'], function()
+Route::group($middleware + ['prefix' => 'Installer@update', 'namespace' => 'Simexis\Installer\Controllers\Update'], function()
 {
     Route::group(['middleware' => 'installerCanUpdate'], function(){
         Route::get('/', [
@@ -39,7 +43,7 @@ Route::group(['prefix' => 'Installer@update', 'namespace' => 'Simexis\Installer\
     });
 });
 
-Route::group(['prefix' => 'Installer@assets', 'namespace' => 'Simexis\Installer\Controllers'], function()
+Route::group($middleware + ['prefix' => 'Installer@assets', 'namespace' => 'Simexis\Installer\Controllers'], function()
 {
     Route::get(sprintf('stylesheets?%s', app('Simexis\Installer\Helpers\Render')->getModifiedTime('css')), [
 		'as' => 'installer::assets.css',
